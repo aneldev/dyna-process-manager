930d3048a97121f7aa3a3e28141327f4829b87c0
dennisat <dennis@anel.co>
2020-03-17 08:38:05 +0100

    impr: Events to lower case

--- src/DynaProcess.ts
+++ src/DynaProcess.ts
@@ -58,13 +58,19 @@ export class DynaProcess extends EventEmitter {
     // For all termination signals, push the to the child.
     // On some system's like Mac OS Catalina update, the children don't get the termination signal always.
     // https://www.gnu.org/software/libc/manual/html_node/Termination-Signals.html#:~:text=The%20(obvious)%20default%20action%20for,cause%20the%20process%20to%20terminate.&text=The%20SIGTERM%20signal%20is%20a,ask%20a%20program%20to%20terminate.
-    [
+    const terminationSignals: string[] = [
       "SIGTERM",
       "SIGINT",
       "SIGQUIT",
-      // "SIGKILL",
       "SIGHUP",
-    ]
+      // "SIGKILL",
+    ];
+
+    (new Array<string>())
+      .concat(
+        terminationSignals,
+        terminationSignals.map(s => s.toLowerCase())
+      )
       .forEach(signal => {
         process.on(signal as Signals, () => {
           console.debug('Passing termination signal', signal);
